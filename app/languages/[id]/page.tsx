fea17930ef7d8f257b4d91bbde02d29e41457361
YangThePerson <87084922+YangThePerson@users.noreply.github.com>
2026-07-01 09:31:09 -0400

    Added handling for overview with invalid languageId

--- app/languages/[id]/page.tsx
+++ app/languages/[id]/page.tsx
@@ -1,7 +1,21 @@
+import { getOrCreateDbUser } from '@/app/lib/current-user';
+import { listLanguagesSvc } from '@/app/lib/languages';
+import { redirect } from 'next/navigation';
+
 export default async function LanguagePage({
   params,
 }: PageProps<'/languages/[id]'>) {
   const { id } = await params;
+
+  const user = await getOrCreateDbUser();
+  if (!user) redirect('/sign-in');
+
+  const languages = await listLanguagesSvc(user);
+  if (
+    !languages.length ||
+    !languages.find(({ id: languageId }) => languageId === id)
+  )
+    redirect('/languages');
 
   return <div>{id}</div>;
 }
